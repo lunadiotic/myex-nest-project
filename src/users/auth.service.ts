a583adb9fe00183e46968acbfae0db5f796fe6e6
aimeliala <idstack@outlook.co.id>
2023-05-19 23:30:03 +0700

    cara membuat salt dan hash untuk password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async register(name: string, email: string, password: string) {
+    //   check apakah ada user yang menggunakan email yang sama
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email sudah terdaftar');
+    }
+
+    // hash password
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 64)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    // create user
+  }
 }
