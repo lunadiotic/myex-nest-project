80db7421d347b28ec26eb7ae03c3fe000bdc0564
aimeliala <idstack@outlook.co.id>
2023-05-20 12:05:13 +0700

    setup session dan cara kerja cookie

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({ keys: ['r4nd0m5tr1n6'] }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
