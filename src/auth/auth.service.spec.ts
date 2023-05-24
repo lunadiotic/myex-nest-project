54e34e1b6d3eb1582b35e01ec9bba0c7442ead93
aimeliala <idstack@outlook.co.id>
2023-05-24 13:23:59 +0700

    menguji method register di auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,20 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
-    const fakeUsersService = {
+    const fakeUsersService: Partial<UsersService> = {
       find: () => Promise.resolve([]),
-      create: () =>
-        Promise.resolve({
-          id: 1,
-          name: 'Test',
-          email: 'test@test',
-          password: 'test',
-        }),
+      create: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -32,5 +29,19 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user', async () => {
+    const user = await service.register(
+      'Jane Doe',
+      'hv3RU@example.com',
+      'password',
+    );
+
+    expect(user.name).toBe('Jane Doe');
+    expect(user.password).not.toEqual('password');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
