76231762a5acb605128a7df5607d71839b1a12e9
aimeliala <idstack@outlook.co.id>
2023-06-07 14:03:21 +0700

    membuat service create item

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
 import { ItemsService } from './items.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('items')
 export class ItemsController {
   constructor(private itemService: ItemsService) {}
+
   @Post()
+  @UseGuards(AuthGuard)
   createItem(@Body() body: CreateItemDto) {
     return this.itemService.create(body);
   }
