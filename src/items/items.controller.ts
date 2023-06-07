2dbaf24864b0988e6521266e25ca3895c74d0842
aimeliala <idstack@outlook.co.id>
2023-06-07 13:41:21 +0700

    menerapkan validator pada item dto

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private itemService: ItemsService) {}
   @Post()
   createItem(@Body() body: CreateItemDto) {
-    return 'This action adds a new item';
+    return this.itemService.create(body);
   }
 }
